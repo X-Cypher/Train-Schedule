37aa3f029dafddaf6ec0f2b8ee4c0e91f8b1d753
Arjit Gautam <arjitgautamw2015@gmail.com>
2025-11-08 18:51:37 +0530

    added comment for understanding

--- Train/src/main/java/com/example/Train/repo/TrainScheduleRepo.java
+++ Train/src/main/java/com/example/Train/repo/TrainScheduleRepo.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface TrainScheduleRepo extends JpaRepository<TrainSchedule, Long> {
     List<TrainSchedule> findBySource_StationCodeAndDestination_StationCode(String sourceCode, String destinationCode);
     List<TrainSchedule> findBySource_StationNameAndDestination_StationName(String sourceName, String destinationName);
+    //underscore is used for column names, so findBySource = source variable of TrainSchedule and then _StationCode means we will now get into Source station and get its stationCode variable
 }
